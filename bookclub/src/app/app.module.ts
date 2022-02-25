825ce755d9cbb1dc848593d6652b1effe8188abb
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-24 22:35:33 -0600

    created the components nav, quote, post, and landing(page) for the frontend

--- bookclub/src/app/app.module.ts
+++ bookclub/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PostComponent } from './post/post.component';
+import { QuoteComponent } from './quote/quote.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PostComponent,
+    QuoteComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
