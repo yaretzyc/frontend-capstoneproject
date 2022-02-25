dc8ff8f7bc5791db04a0824c1efb25a2fe44c2d4
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-25 07:49:07 -0600

    installed bootstrap

--- bookclub/src/app/services/book.service.ts
+++ bookclub/src/app/services/book.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  // private baseURL = "http://localhost:8080/api/book/books/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  // getBookList(): Observable<Book[]>{
+  //   return this.httpClient.get<Book[]>(`${this.baseURL}`);
+  // }
 }
