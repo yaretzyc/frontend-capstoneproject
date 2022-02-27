65863cd62449d2e566f702b75ef6f47ac352de4a
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-26 23:27:43 -0600

    created the book.ts file to store the field names that match the backend fields for the book model

---
+++ bookclub/src/app/book.ts
@@ -0,0 +1,13 @@
+export class Book{
+
+    id: number=0;
+    title: string='';
+    author: string = '';
+    img: string = '';
+    summary: string='';
+    link: string = '';
+    pages: string = '';
+    genre: string = '';
+    other: string = '';
+
+}
