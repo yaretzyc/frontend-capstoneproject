4c78488025e5eb0d577e4a6d8f9df04b9d026d41
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-26 23:34:33 -0600

    added the book service.ts file to do crud

--- bookclub/src/app/services/book.service.ts
+++ bookclub/src/app/services/book.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Book } from '../book';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,38 @@ import { Observable } from 'rxjs';
 export class BookService {
 
   // private baseURL = "http://localhost:8080/api/book/books/"
+  private apibaseUrl = environment.baseUrl;
 
   constructor(private httpClient: HttpClient) { }
 
   // getBookList(): Observable<Book[]>{
   //   return this.httpClient.get<Book[]>(`${this.baseURL}`);
   // }
+
+  
+    //GET ALL Books
+    getPostList(): Observable<Book[]>{
+      return this.httpClient.get<Book[]>(`${this.apibaseUrl}/books/`);
+    }
+    
+    //ADD Book
+    addPost(Book: Book): Observable<Object> {
+      return this.httpClient.post<Book>(`${this.apibaseUrl}/book/`, Book);
+    }
+    
+    // //UPDATE Book
+    updatePost(postId:number, Book: Book ): Observable<Object>{
+      return this.httpClient.put<Book>(`${this.apibaseUrl}/book/${postId}/`, Book);
+    }
+  
+    //DELETE Book
+    deletePost(postId: number): Observable<void>{
+      return this.httpClient.delete<void>(`${this.apibaseUrl}/book/${postId}/`);
+    }
+  
+  
+    //GET ONE Book: 
+    getOnePost(postId: number): Observable<Book>{
+      return this.httpClient.get<Book>(`${this.apibaseUrl}/book/${postId}/`);
+    }
 }
