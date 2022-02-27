22e485494d0bf38602fb873fc77e0b45dff64d3f
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-26 21:58:39 -0600

    adding the post.ts file with the field names from from the backend

---
+++ bookclub/src/app/post.ts
@@ -0,0 +1,13 @@
+export class Quote {
+    
+    id: number = 0;
+    title: string ='';
+    name: string = '';
+    body: string = '';
+    page: any;
+    LocalDate: any;
+    LocalDateTime: any;
+
+
+
+}
