ff2f32ec56fe285d004ddc53e90ac08bd5edf7a7
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-27 19:10:49 -0600

    added the edit post component to edit a post details

--- bookclub/src/app/edit-post/edit-post.component.ts
+++ bookclub/src/app/edit-post/edit-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostService } from '../services/post.service';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-edit-post',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPostComponent implements OnInit {
 
-  constructor() { }
+  postId: any;
+  post: Post = new Post();
+
+  constructor(private postService: PostService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.postId = this.route.snapshot.params['postId'];
+    this.postService.getOnePost(this.postId).subscribe(data =>{
+      this.post = data;
+      console.log(data);
+    })
+
+  }
+
+  onSubmit(){
+    this.postService.updatePost(this.postId, this.post).subscribe( data =>{
+      this.router.navigate(['/post']);
+    }
+    ,error => console.log(error));
   }
 
 }
