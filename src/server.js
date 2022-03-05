92e3e79afe03c6db6ad52bad5d532af91bf58724
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-03-04 19:10:38 -0600

    set the port

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 8080
+
 const app = express();
 
+//set the port
+app.set("port", PORT);
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/frontend-capstoneproject'));
 
